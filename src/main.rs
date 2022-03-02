99aeed31d6e61a32a0b820c75bc78887f0ecbc71
Vizdun <55970818+Vizdun@users.noreply.github.com>
2022-03-02 15:29:14 +0100

    initial

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::framework::standard::{
+    macros::{command, group},
+    CommandResult, StandardFramework,
+};
+use serenity::model::channel::Message;
+
+use std::env;
+
+#[group]
+#[commands(roll)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn roll(ctx: &Context, msg: &Message) -> CommandResult {
+    use roll_rs::roll_inline;
+
+    msg.reply(
+        ctx,
+        roll_inline(msg.content.splitn(2, " ").collect::<Vec<&str>>()[1], false)
+            .unwrap()
+            .string_result,
+    )
+    .await?;
+
+    Ok(())
+}
